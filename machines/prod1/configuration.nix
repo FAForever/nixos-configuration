58ac1f51239eb6abe42fcd42e006ce2880b0c48b
FAForever admins <admin@faforever.com>
2023-05-01 08:18:53 +0000

    Add restic backups to rsync.net

--- machines/prod1/configuration.nix
+++ machines/prod1/configuration.nix
@@ -82,6 +82,70 @@
         verbose
       '';
     };
+
+    # == Backups to an external services with restic ==
+    #
+    # The password in /etc/restic-password is also secured in our Vaultwarden.
+    # This config requires an ssh-config for rsync.net for a username and host using <username>.rsync.net.
+    # Also, the public key of this root user must be placed in rsync.net (.ssh/authorized_keys)
+
+    restic.backups = {
+      backup_config = {
+        passwordFile = "/etc/restic-password";
+        paths = [
+          "/opt/faf/config"
+        ];
+        repository = "sftp:@rsync.net:restic/config";
+        timerConfig = {
+          OnCalendar = "04:00";
+        };
+      };
+
+      maps = {
+        passwordFile = "/etc/restic-password";
+        paths = [
+          "/opt/faf/data/content/maps"
+        ];
+        repository = "sftp:@rsync.net:restic/maps";
+        timerConfig = {
+          OnCalendar = "04:05";
+        };
+      };
+
+      mods = {
+        passwordFile = "/etc/restic-password";
+        paths = [
+          "/opt/faf/data/content/mods"
+        ];
+        repository = "sftp:@rsync.net:restic/mods";
+        timerConfig = {
+          OnCalendar = "04:15";
+        };
+      };
+
+      featured_mods = {
+        passwordFile = "/etc/restic-password";
+        paths = [
+          "/opt/faf/data/content/legacy-featured-mod-files"
+        ];
+        repository = "sftp:@rsync.net:restic/featured_mods";
+        timerConfig = {
+          OnCalendar = "04:30";
+        };
+      };
+
+      nodebb_uploads = {
+        passwordFile = "/etc/restic-password";
+        paths = [
+          "/opt/faf/data/nodebb/uploads"
+        ];
+        repository = "sftp:@rsync.net:restic/nodebb_uploads";
+        timerConfig = {
+          OnCalendar = "04:45";
+        };
+      };
+      
+    };
   };
 
   # This value determines the NixOS release with which your system is to be
