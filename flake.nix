b3a2c4984b8882a830873a3e05636f2d27fe88b0
FAForever admins <admin@faforever.com>
2024-10-03 20:41:40 +0000

    Initial flake setup, WIP

--- flake.nix
+++ flake.nix
@@ -5,9 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     #nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     srvos.url = "github:nix-community/srvos";
+    secrets = {
+      url = "git+file:secrets"; # the submodule is in the ./secrets dir
+      flake = false;
+    };
+
   };
 
-  outputs = { self, nixpkgs, srvos }: {
+  outputs = { self, nixpkgs, secrets, srvos }: {
     nixosConfigurations = {
       "fafprod3" = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
